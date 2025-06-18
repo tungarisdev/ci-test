f229c968bda0075ad3dea0805248b8269c46d971
tungarisdev <tungaris.dev@gmail.com>
2025-06-18 22:35:07 +0700

    hmm

--- src/deploy.bat
+++ src/deploy.bat
@@ -6,26 +6,26 @@ set TOKEN=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
 set DOWNLOAD_DIR=D:\deploy\artifact
 set TOMCAT_WEBAPPS_PATH=D:\Tomcat\apache-tomcat-10.1.25\webapps
 
-echo === Lấy RUN_ID mới nhất ===
+echo === LAY RUN ID MOI NHAT ===
 for /f "usebackq tokens=*" %%i in (`curl -s -H "Authorization: token %TOKEN%" https://api.github.com/repos/%REPO_OWNER%/%REPO_NAME%/actions/runs ^| jq ".workflow_runs[0].id"`) do set RUN_ID=%%i
 
-echo === Lấy Artifact URL ===
+echo === LAY ARTIFACT URL ===
 for /f "usebackq tokens=*" %%i in (`curl -s -H "Authorization: token %TOKEN%" https://api.github.com/repos/%REPO_OWNER%/%REPO_NAME%/actions/runs/%RUN_ID%/artifacts ^| jq -r ".artifacts[] | select(.name==\"%ARTIFACT_NAME%\") | .archive_download_url"`) do set ARTIFACT_URL=%%i
 
-echo === Tải Artifact ===
+echo === TAI ARTIFACT ===
 mkdir "%DOWNLOAD_DIR%" 2>nul
 cd /d "%DOWNLOAD_DIR%"
 curl -L -H "Authorization: token %TOKEN%" %ARTIFACT_URL% -o artifact.zip
 
-echo === Giải nén ===
+echo === GIAI NEN ===
 tar -xf artifact.zip
 
-echo === Tìm WAR file và copy ===
+echo === TIM WAR FILE VA COPY ===
 for /r %%f in (*.war) do (
     copy /Y "%%f" "%TOMCAT_WEBAPPS_PATH%\ci-test.war"
 )
 
-echo === WAR đã copy. ===
+echo === WAR DA COPY. ===
 REM Bạn có thể restart Tomcat bằng cách gọi service, nếu có cài:
 REM net stop Tomcat10 && net start Tomcat10
 
