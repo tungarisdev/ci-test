b46a1464384bc13adcf4f09578a1781822ecf568
tungarisdev <tungaris.dev@gmail.com>
2025-06-26 14:42:27 +0700

    TEST CI/CD

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Butt!";
+        return "Hello from Spring But!";
     }
 }
