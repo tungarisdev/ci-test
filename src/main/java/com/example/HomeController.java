6854a6a6ae2ef6dc86fcdf4e42615cf45a467896
tidy-tado <qtide777@gmail.com>
2025-06-26 15:12:27 +0700

    Them controller /thai

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -9,4 +9,9 @@ public class HomeController {
     public String hello() {
         return "Hello from Spring But!";
     }
+@GetMapping("/thai")
+    public String hello() {
+        return "Day la nhanh cua thai!";
+    }
+
 }
